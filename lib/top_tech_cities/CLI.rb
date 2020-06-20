08bd71d2537f3d723989d7a6754b643df0973aed
Arodriguez1030 <arodriguez1030@gmail.com>
2020-06-20 18:07:41 -0400

    color

--- lib/top_tech_cities/CLI.rb
+++ lib/top_tech_cities/CLI.rb
@@ -1,9 +1,10 @@
 require "pry"
+require "colorize"
 
 class TopTechCities::Cli
     
     def welcome
-        puts "Welcome to Top Tech Cities! This is a list of the Top cities hiring Tech careers"
-        binding.pry
+        puts "Welcome to Top Tech Cities! This is a list of the Top cities hiring Tech careers.".green
+        puts "Enter the number of the city, or the city name, that you're interested in learning more about.".green
     end
 end
